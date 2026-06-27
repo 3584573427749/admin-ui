83d15d5114ae05ad71af187d1ac6d0950b899d36
Kjell <kjell@kejpa.com>
2026-06-27 15:32:22 +0300

    Lagt till vuetify och uppdaterat versioner

--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@ import { createPinia } from 'pinia';
 import router from './router';
 import App from './App.vue';
 
+import vuetify from './plugins/vuetify';
+
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(vuetify);
 app.use(router);
 
 app.mount('#app');
